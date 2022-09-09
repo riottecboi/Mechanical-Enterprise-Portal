32e452c6b5419837db1f6348600e11fe6a7be259
riottecboi <tranvinhliem1307@gmail.com>
2022-09-10 00:07:41 +0700

    Create table based on Excel file

--- app/database.py
+++ app/database.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
+from app.utils import extracted_excel_file
+from sqlalchemy import Table, Column, Integer, String, Float, DateTime
+import pandas
 import os
 
 load_dotenv()
@@ -15,8 +18,17 @@ db_url = {
     'query': {'charset': 'utf8'},  # the key-point setting
 }
 
+columns = {}
+convention = {int: Integer, float: Float, str: String(255), pandas.Timestamp: DateTime}
+
+excel_columns = extracted_excel_file(os.getenv('EXCEL_PATH'))[1][0]
+
 engine = create_engine(os.getenv('DATABASE_URL'), encoding='utf8')
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+for k, v in excel_columns.items():
+    columns[k] = convention[type(v)]
+fields = (Column(colname, coltype) for colname, coltype in columns.items())
+t = Table('U', Base.metadata, *fields)
