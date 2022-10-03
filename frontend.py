7112c89de25487663193ffcf0916e1e6c0724f0e
vinhliem <liem@dfir.vn>
2022-10-03 22:10:10 +0700

    Add new single user

--- frontend.py
+++ frontend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, abort
 from flask_wtf.csrf import CSRFProtect
 from flask_env import MetaFlaskEnv
 from datetime import timedelta
@@ -20,6 +20,11 @@ csrf = CSRFProtect(app)
 
 app.config['SESSION_COOKIE_SECURE']=True
 app.permanent_session_lifetime = timedelta(minutes=30)
+
+@app.errorhandler(403)
+def session_timeout(error):
+    flash('Phiên đăng nhập hết hạn', 'warning')
+    return redirect(url_for('login'), 302)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -60,6 +65,8 @@ def forgot():
 def menu():
     userAdmin = session.get('admin')
     userAPIkey = session.get('apikey')
+    if userAPIkey is None:
+        abort(403, description="Phiên đăng nhập hết hạn")
     userInfo = requests.get(f"{app.config['BASE_URL']}/info", params={'apikey': userAPIkey}, headers={'X-SECRET-KEY': session.get('apikey')})
     if userAdmin is True:
         getAllusers = requests.get(f"{app.config['BASE_URL']}/allUser", headers={'X-ADMIN-SECRET-KEY': session.get('apikey')})
@@ -76,6 +83,60 @@ def adminInfo():
     userInfo = requests.get(f"{app.config['BASE_URL']}/info", params={'apikey': userAPIkey},
                             headers={'X-SECRET-KEY': session.get('apikey')})
     return render_template('profile.html', cur_user=userInfo.json(), userType='Administrator')
+
+@app.route('/adduser', methods=['GET','POST'])
+def adduser():
+    if request.method == 'POST':
+        msnv = request.form.get('msnv')
+        fullname = request.form.get('fullname')
+        dob = request.form.get('dob')
+        gender = request.form.get('gender')
+        tel = request.form.get('tel')
+        idcard = request.form.get('idcard')
+        ethnic = request.form.get('ethnic')
+        nationality = request.form.get('nationality')
+        address = request.form.get('address')
+        ward = request.form.get('ward')
+        district = request.form.get('district')
+        city = request.form.get('city')
+        group = request.form.get('group')
+        sector = request.form.get('sector')
+        title = request.form.get('title')
+        position = request.form.get('position')
+        unit = request.form.get('unit')
+        newpw = request.form.get('newpw')
+        re_newpw = request.form.get('re-newpw')
+        if newpw == re_newpw:
+            json_d = {
+                'username': msnv,
+                'password': newpw,
+                'confirm_password': re_newpw,
+                'msnv': msnv,
+                'fullname': fullname,
+                'department': unit,
+                'gender': gender,
+                'vehicle': title,
+                'position': position,
+                'dob': dob,
+                'sector': sector,
+                'tel': tel,
+                'id_card': idcard,
+                'ethnic': ethnic,
+                'nationality': nationality,
+                'address': address,
+                'ward': ward,
+                'district': district,
+                'city': city,
+                'target_group': group
+            }
+            r_signup = requests.post(f"{app.config['BASE_URL']}/signup",  json=json_d, headers={'X-ADMIN-SECRET-KEY': session.get('apikey')})
+            if r_signup.status_code == 200:
+                flash('Tạo account mới thành công', 'info')
+                return redirect(url_for('adduser'))
+            else:
+                flash('Không tạo được account', 'error')
+                return redirect(url_for('adduser'))
+    return render_template('adduser.html', userType='Administrator')
 
 @app.route('/logout', methods=['GET'])
 def logout():
